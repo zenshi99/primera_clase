e10246fa75fb8725650599955f8ab046f967384a
zenshi99 <jorgitoquira15@gmail.com>
2026-07-14 10:22:07 -0500

    cambios

---
+++ ejercicio_03/script.js
@@ -0,0 +1,7 @@
+//es como el print en python
+alert("¡Hola, mundo!");
+//es como el input en python
+numero=prompt("¿Cuál es tu nombre?");
+alert("Hola " + numero + " bienvenido a la clase de javascript");
+//podemos usar al igual que python el texto por terminal/con solo
+console.log(numero);
